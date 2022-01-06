e07c2588e4abad06a8f6d1a161d3010ffcaeec6f
Jessica Sarai <jessicabautista1698@gmail.com>
2022-01-06 00:48:22 -0600

    query methods

--- platzi-market/src/main/java/com/platzi/market/persistencia/ProductoRepository.java
+++ platzi-market/src/main/java/com/platzi/market/persistencia/ProductoRepository.java
@@ -12,4 +12,15 @@ public class ProductoRepository {
     public List<Producto> getAll(){  //hay que castear
         return (List<Producto>) productoCrudRepository.findAll();
     }
+
+    public List<Producto> getByCategoria(int idCategoria){
+        /*Se nombra como está en el CrudRepository - devuelve una lista de productos con la categoria especifica
+        ordenador en orden alfabetic*/
+        return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria); 
+        
+    }
+
+    public Optional<List<Producto>> getEscasos(int cantidad){
+        return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true); 
+    }
 }
--- platzi-market/src/main/java/com/platzi/market/persistencia/crud/ProductoCrudRepository.java
+++ platzi-market/src/main/java/com/platzi/market/persistencia/crud/ProductoCrudRepository.java
@@ -3,5 +3,16 @@ package com.platzi.market.persistencia.crud;
 import com.platzi.market.persistencia.entity.Producto;
 import org.springframework.data.repository.CrudRepository;
 
+//Aqui se crean los query methods
 public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+    /*escribiendo un nuevo metodo - recuperar toda la lista de prod que pertenezcan a una categoria en especifico
+    nomenclatura: findBy_AtributoABuscar, y se manda el atributo como esté en la clase.*/
+    List <Producto> findByIdCategoriaOrderByNombreAsc(int  idCategoria); 
+
+    /*De otra manera ingresando con Query sería de la sig manera
+    @Query("SELECT * FROM productos where id_categoria = ?", nativeQuery = true) //ya no es necesario llamar el método como findById
+    List<Producto> getByCategoria(int idCategoria); */
+
+    //recueprar productos escasos
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado); 
 }
